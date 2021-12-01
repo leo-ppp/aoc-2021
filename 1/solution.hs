365c20118489a1eca51a208327ee375ae06dc9d6
Leo Poulson <leo.poulson@droitfintech.com>
2021-12-01 13:21:06 +0000

    pt1

--- 1/solution.hs
+++ 1/solution.hs
@@ -1,5 +1,19 @@
+isIncrease :: Ord a => a -> a -> Bool
+isIncrease x y = x < y
 
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs [_] = []
+pairs (a : b : as) = (a, b) : pairs (b : as)
 
+totalIncreases :: Ord a => [a] -> Int
+totalIncreases = length . filter (uncurry isIncrease) . pairs
 
-readNumbers :: String -> IO [Integer]
+-- reading / testing
+
+readNumbers :: String -> IO [Int]
 readNumbers fileName = map read . lines <$> readFile fileName
+
+testNumbers = [199,200,208,210,200,207,240,269,260,263]
+
+testAnswer = 7 == totalIncreases testNumbers
